6ec09599efd3c180132096aa18ae50c0d8e49e4d
Matti Luukkainen <mluukkai@iki.fi>
2025-10-12 19:53:40 +0300

    nhlstats

---
+++ viikko1/nhl-statistics/src/index.py
@@ -0,0 +1,19 @@
+from statistics_service import StatisticsService
+
+
+def main():
+    stats = StatisticsService()
+    philadelphia_flyers_players = stats.team("PHI")
+    top_scorers = stats.top(10)
+
+    print("Philadelphia Flyers:")
+    for player in philadelphia_flyers_players:
+        print(player)
+
+    print("Top point getters:")
+    for player in top_scorers:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
